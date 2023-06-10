e0d23357609fe9dca0d947e6eb0cc95f110f87ae
Ailakks <hola@cortmnzz.com>
2023-06-11 00:20:56 +0200

    initial commit

---
+++ sunlatte-spigot/src/main/java/net/hyphaelands/sunlatte/SunlatteSpigot.java
@@ -0,0 +1,17 @@
+package net.hyphaelands.sunlatte;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SunlatteSpigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ sunlatte-velocity/src/main/java/net/hyphaelands/sunlatte/SunlatteVelocity.java
@@ -0,0 +1,24 @@
+package net.hyphaelands.sunlatte;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(
+        id = "sunlatte-velocity",
+        name = "Sunlatte",
+        version = "@version@",
+        url = "https://hyphaelands.net",
+        authors = {"Hyphaelands"}
+)
+public class SunlatteVelocity {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
