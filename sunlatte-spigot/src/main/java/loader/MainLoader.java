04b0cc78ff1358573700807d80ecb47b0b229ef0
Ailakks <hola@cortmnzz.com>
2023-06-11 18:25:12 +0200

    chore: file author header

--- sunlatte-spigot/src/main/java/loader/MainLoader.java
+++ sunlatte-spigot/src/main/java/loader/MainLoader.java
@@ -1,3 +1,13 @@
+/**
+ * This code has been programmed by Ailakks.
+ * Please, leave this note and give me credits
+ * in any project in which it is used. Have a nice day!
+ *
+ * @author : Ailakks
+ * @mailto : hola@ailakks.com
+ * @created : 11/06/2023
+ */
+
 package loader;
 
 import com.google.inject.Inject;
