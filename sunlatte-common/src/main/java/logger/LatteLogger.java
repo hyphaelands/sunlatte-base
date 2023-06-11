599b90090cb64481875cb38935d4e15e2be0dbb9
Ailakks <hola@cortmnzz.com>
2023-06-11 19:39:31 +0200

    feat: latte logger

---
+++ sunlatte-common/src/main/java/logger/LatteLogger.java
@@ -0,0 +1,32 @@
+/**
+ * This code has been programmed by Ailakks.
+ * Please, leave this note and give me credits
+ * in any project in which it is used. Have a nice day!
+ *
+ * @author : Ailakks
+ * @mailto : hola@ailakks.com
+ * @created : 11/06/2023
+ */
+
+package logger;
+
+import lombok.Data;
+
+import java.util.logging.Logger;
+
+@Data
+public class LatteLogger {
+    private static final Logger logger = Logger.getLogger("Latte");
+
+    public void info(String message) {
+        logger.info(message);
+    }
+
+    public void warning(String message) {
+        logger.warning(message);
+    }
+
+    public void severe(String message) {
+        logger.severe(message);
+    }
+}
