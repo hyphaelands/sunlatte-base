bff94a88f1449bcd0b134b38b7799697aad64a39
Ailakks <hola@cortmnzz.com>
2023-06-12 00:29:45 +0200

    feat: bukkit menu

---
+++ sunlatte-spigot/src/main/java/net/hyphaelands/sunlatte/menu/BukkitMenu.java
@@ -0,0 +1,34 @@
+/**
+ * This code has been programmed by Ailakks.
+ * Please, leave this note and give me credits
+ * in any project in which it is used. Have a nice day!
+ *
+ * @author : Ailakks
+ * @mailto : hola@ailakks.com
+ * @created : 12/06/2023
+ */
+
+package net.hyphaelands.sunlatte.menu;
+
+import lombok.Data;
+import net.hyphaelands.sunlatte.player.LatteBukkitPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+
+import java.util.HashMap;
+import java.util.function.Consumer;
+
+@Data
+public class BukkitMenu {
+    private static final HashMap<Integer, Consumer<InventoryClickEvent>> actionHashMap = new HashMap<>();
+
+    private final LatteBukkitPlayer latteBukkitPlayer;
+    private final Inventory bukkitInventory;
+
+    public BukkitMenu(LatteBukkitPlayer latteBukkitPlayer) {
+        this.latteBukkitPlayer = latteBukkitPlayer;
+
+        this.bukkitInventory = Bukkit.createInventory(latteBukkitPlayer, 1);
+    }
+}
