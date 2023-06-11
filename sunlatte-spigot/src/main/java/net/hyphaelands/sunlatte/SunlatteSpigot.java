2434c7a8048b8f5ae64fd36061bcd646fbff469f
Ailakks <hola@cortmnzz.com>
2023-06-11 14:46:24 +0200

    feat: latte spigot instance

--- sunlatte-spigot/src/main/java/net/hyphaelands/sunlatte/SunlatteSpigot.java
+++ sunlatte-spigot/src/main/java/net/hyphaelands/sunlatte/SunlatteSpigot.java
@@ -1,12 +1,17 @@
 package net.hyphaelands.sunlatte;
 
+import lombok.Data;
 import net.hyphaelands.sunlatte.listener.latte.LattePlayerJoinListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Data
 public final class SunlatteSpigot extends JavaPlugin {
+    private static SunlatteSpigot instance;
 
     @Override
     public void onEnable() {
+        instance = this;
+
         this.getServer().getPluginManager().registerEvents(new LattePlayerJoinListener(), this);
     }
 
---
+++ sunlatte-spigot/src/main/java/net/hyphaelands/sunlatte/listener/bukkit/PlayerJoinListener.java
@@ -0,0 +1,13 @@
+package net.hyphaelands.sunlatte.listener.bukkit;
+
+import net.hyphaelands.sunlatte.SunlatteSpigot;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoinListener implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        SunlatteSpigot.
+    }
+}
