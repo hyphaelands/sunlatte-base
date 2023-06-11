b7af2293e5eddb71d66fa90433d2eff7a4fa1da9
Ailakks <hola@cortmnzz.com>
2023-06-11 18:10:44 +0200

    refactor: main loader

--- sunlatte-spigot/src/main/java/loader/MainLoader.java
+++ sunlatte-spigot/src/main/java/loader/MainLoader.java
@@ -3,11 +3,14 @@ package loader;
 import com.google.inject.Inject;
 import loader.loader.ListenerLoader;
 
+import java.util.Collections;
+
 public class MainLoader implements ILoader {
     @Inject ListenerLoader listenerLoader;
 
     @Override
     public void load() {
-        listenerLoader.load();
+        Collections.singletonList(listenerLoader)
+                .forEach(listener -> listenerLoader.load());
     }
 }
