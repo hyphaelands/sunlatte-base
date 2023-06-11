ab77fecbd4709b500e3d20ce206bdd2d854be7af
Ailakks <hola@cortmnzz.com>
2023-06-11 19:16:41 +0200

    feat: latte bukkit player message provider

--- sunlatte-spigot/src/main/java/net/hyphaelands/sunlatte/player/LatteBukkitPlayer.java
+++ sunlatte-spigot/src/main/java/net/hyphaelands/sunlatte/player/LatteBukkitPlayer.java
@@ -10,18 +10,22 @@
 
 package net.hyphaelands.sunlatte.player;
 
+import com.google.inject.Inject;
 import org.bukkit.entity.Player;
 import player.LattePlayer;
+import translation.LatteMessageProvider;
 
 import java.util.UUID;
 
 public abstract class LatteBukkitPlayer extends LattePlayer implements Player {
+    @Inject LatteMessageProvider latteMessageProvider;
+
     public LatteBukkitPlayer(UUID uuid) {
         super(uuid);
     }
 
     @Override
     public void sendTranslatedMessage(String key) {
-
+        this.latteMessageProvider.provide().send(this, key);
     }
 }
